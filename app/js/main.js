666fc37b6abeabfcdf29699bbcdefc49f1422754
Nikita <nikitamalashkin73@gmail.com>
2023-04-11 08:10:35 +0600

    added login register 404 pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 new Swiper('.swiper', {
   pagination: {
     el: '.swiper-pagination',
@@ -109,3 +111,130 @@ document.addEventListener('keydown', function (e) {
     popupClose(popupActive)
   }
 })
+// ----------------------------------------------------------------------
+
+const ratings = document.querySelectorAll('.rating')
+if (ratings.length > 0) {
+  initRatings()
+}
+
+// Main function
+function initRatings() {
+  let ratingActive, ratingValue
+  for (let index = 0; index < ratings.length; index++) {
+    const rating = ratings[index]
+    initRating(rating)
+  }
+
+  // инициализация конкретного рейтинга
+  function initRating(rating) {
+    initRatingVars(rating)
+
+    setRatingActiveWidth()
+
+    if (rating.classList.contains('rating--set')) {
+      setRating(rating)
+    }
+  }
+
+  // инициализация переменных
+  function initRatingVars(rating) {
+    ratingActive = rating.querySelector('.rating__active')
+    ratingValue = rating.querySelector('.rating__value')
+  }
+
+  // изменение ширины активных звезд
+  function setRatingActiveWidth(index = ratingValue.innerHTML) {
+    const ratingActiveWidth = index / 0.05
+    ratingActive.style.width = `${ratingActiveWidth}%`
+  }
+
+  // возможность указать оценку
+  function setRating(rating) {
+    const ratingItems = rating.querySelectorAll('.rating__item')
+    // for (let index = 0; index < ratingItems.length; index++) {
+    //   const ratingItem = ratingItems[index]
+    //   ratingItem.addEventListener('mouseenter', function (e) {
+    //     обновление переменных
+    //     initRatingVars(rating)
+    //     обновление активных звезд
+    //     setRatingActiveWidth(ratingItem.value)
+    //   })
+    //   ratingItem.addEventListener('mouseleave', function (e) {
+    //     // обнуление активных звезд
+    //     setRatingActiveWidth()
+    //   })
+    //   ratingItem.addEventListener('click', function (e) {
+    //     //обнуление пременных
+    //     initRatingVars(rating)
+
+    //     if (rating.dataset.ajax) {
+    //       // отправить на сервер
+    //       setRatingValue(ratingItem.value, rating)
+    //     } else {
+    //       // отобразить указанную оценку
+    //       ratingValue.innerHTML = index + 1
+    //       setRatingActiveWidth()
+    //     }
+    //   })
+    // }
+  }
+}
+
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date())
+  const seconds = Math.floor((total / 1000) % 60)
+  const minutes = Math.floor((total / 1000 / 60) % 60)
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24)
+  const days = Math.floor(total / (1000 * 60 * 60 * 24))
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds,
+  }
+}
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date())
+  const seconds = Math.floor((total / 1000) % 60)
+  const minutes = Math.floor((total / 1000 / 60) % 60)
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24)
+  const days = Math.floor(total / (1000 * 60 * 60 * 24))
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds,
+  }
+}
+
+function initializeClock(id, endtime) {
+  const clock = document.getElementById(id)
+  const daysSpan = clock.querySelector('.days')
+  const hoursSpan = clock.querySelector('.hours')
+  const minutesSpan = clock.querySelector('.minutes')
+  const secondsSpan = clock.querySelector('.seconds')
+
+  function updateClock() {
+    const t = getTimeRemaining(endtime)
+
+    daysSpan.innerHTML = t.days
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2)
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2)
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2)
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval)
+    }
+  }
+
+  updateClock()
+  const timeinterval = setInterval(updateClock, 1000)
+}
+
+const deadline = '2023-04-04'
+initializeClock('clock', deadline)
